9d740923d7659f5f8e419677b57e2bc4d89445e0
Louis <louismmaduabuchi1@gmail.com>
2024-12-19 23:32:13 +0000

    K8s Provision

--- gke-worker-node.tf
+++ gke-worker-node.tf
@@ -1,4 +1,4 @@
-resource "google_container_cluster" "Virtualfusion GKE cluster" {
+resource "google_container_cluster" "Virtualfusion-GKE-cluster" {
   name     = var.cluster_name
   location = var.region
   network  = google_compute_network.vpc.id
